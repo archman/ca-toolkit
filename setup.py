ac94d28b07a28f69d5e0a9f51d3199718d82e897
Tong Zhang <zhangt@frib.msu.edu>
2018-08-21 18:56:39 -0400

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, Extension
+
+
+modules = [
+    Extension('ca_toolkit.evolve', ['src/mod_evolve.c', 'src/evolve.c']),
+]
+
+setup(
+    name='ca_toolkit',
+    version='0.1',
+    description='Python toolkit for Cellular Automata',
+    author="Tong Zhang",
+    author_email="warriorlance@gmail.com",
+    ext_modules=modules,
+    packages=['ca_toolkit'],
+    install_requires=['matplotlib', 'numpy'],
+)
